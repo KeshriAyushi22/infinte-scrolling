a660dd371183098cd4924f29a7c73a5fb4b48c7d
AyushiKeshri <keshri.ayushi10@gmail.com>
2021-01-27 23:40:34 +0530

    display of image to center

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,6 +4,7 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import Post from './Post';
 import { Grid } from '@material-ui/core';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import '../App.css'
 
 export class ImageList extends Component {
     state = {
@@ -16,24 +17,19 @@ export class ImageList extends Component {
     getData = (pageNum) => {
         axios
             .get(`photos?client_id=${process.env.REACT_APP_ACCESS_KEY}&&page=${pageNum}`)
-            .then(res => this.setState({ images: res.data }))
+            .then(res => this.setState({ images: this.state.images.concat(res.data) }))
             .catch(err => console.log(err))
 
     }
     componentDidMount() {
         const { count, start } = this.state;
         this.getData(start)
-        console.log(this.state)
     }
 
     fetchImages = () => {
         const { count, start } = this.state;
         this.setState({ start: this.state.start + count });
-        axios
-            .get(`photos?client_id=${process.env.REACT_APP_ACCESS_KEY}&&page=${start}`)
-            .then(res =>
-                this.setState({ images: this.state.images.concat(res.data) })
-            );
+        this.getData(start)
     };
 
     render() {
@@ -44,7 +40,8 @@ export class ImageList extends Component {
                     next={this.fetchImages}
                     hasMore={true}
                     loader={<CircularProgress />}
-                    className='infinite__scroller'
+                    style={{ overflow: 'hidden' }}
+                    className="infinite__scroller"
                 >
                     {this.state.images.map(image => (
                         <Post key={image.id} postDetail={image} />
